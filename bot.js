4cce7d61531551022589635d290c2ff1ec1c0da0
mranh3119 <60533376+mranh3119@users.noreply.github.com>
2020-02-29 02:45:07 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Brodcast Bot ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |DF_Team`,"http://twitch.tv/Death Shop")
+client.user.setGame(`NyE Streaming`,"https://www.twitch.tv/nye_e7")
 client.user.setStatus("dnd")
 });
 
